5ed6c482ad648f9be39329ad21d63905b1946f3d
mujina93 <mujina@hotmail.it>
2018-09-29 02:08:10 +0200

    comment system

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page=settings.LOGIN_REDIRECT_URL), name='logout'),
     path('blog/', include('blog.urls')),
     path('polls/', include('polls.urls')),
 ]
